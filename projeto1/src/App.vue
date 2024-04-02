848d2b39671daacf667b4478a3b13b20cde71656
Jorge Andrade <124082372+jorgeoosthuizen@users.noreply.github.com>
2024-04-02 20:34:15 +0100

    a

--- projeto1/src/App.vue
+++ projeto1/src/App.vue
@@ -14,8 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/counter">Counter</RouterLink>
-
-      
+        <RouterLink to="/pessoas">People</RouterLink>
       </nav>
     </div>
   </header>
--- projeto1/src/views/CounterView.vue
+++ projeto1/src/views/CounterView.vue
@@ -1,22 +1,22 @@
-<template>
-    <div class="about">
-      <h1>This is the counter page</h1>
-      <button @click="counter.increment()"> Clique para incrementar</button>
-      <p>O valor do contador é {{ counter.count }}</p>
-    </div>
-  </template>
-  
-  <style>
-  @media (min-width: 1024px) {
-    .about {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
-  }
-  </style>
+<script setup>
+    import {useCounterStore} from '@/stores/counter'
+    const counter =useCounterStore()
 
-  <script>
-  import {useCounterStore} from '@/stores/counter'
-  const counter = useCounterStore()
 </script>
+<template>
+  <div class="about">
+    <h1>This is an app Counter page</h1>
+    <button @click="counter.increment()">Clique aqui para incrementar</button> 
+    <p>O valor do contador e {{ counter.count }}</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
