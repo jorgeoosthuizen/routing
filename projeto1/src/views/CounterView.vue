17444b19c30f450141b35f64215d0da02acc35e1
Jorge Andrade <124082372+jorgeoosthuizen@users.noreply.github.com>
2024-04-02 20:32:56 +0100

    a

---
+++ projeto1/src/views/CounterView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="about">
+      <h1>This is the counter page</h1>
+      <button @click="counter.increment()"> Clique para incrementar</button>
+      <p>O valor do contador é {{ counter.count }}</p>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+
+  <script>
+  import {useCounterStore} from '@/stores/counter'
+  const counter = useCounterStore()
+</script>
